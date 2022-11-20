674e5331708bcc241a7af74a19692f9372d9a430
Satlay1512 <115040453+Satlay1512@users.noreply.github.com>
2022-11-20 21:17:49 +0300

    Add HW_6

---
+++ HW6_001/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Prompt(string strMessage)
+{
+    System.Console.Write(strMessage);
+    int temp = int.Parse(System.Console.ReadLine());
+    return temp;
+}
+
+int[] CreateArray(int length)
+{
+    int[] tempArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        tempArray[i] = Prompt($"Введите число М  ");
+    }
+    return tempArray;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+
+    }
+
+
+}
+
+int Positive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int len = Prompt($"Введите количество чисел  ");
+int[] array = CreateArray(len);
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"Кличество чисел > 0 = {Positive(array)}");
---
+++ HW6_002/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения
+// b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+const int K = 0;
+const int B = 1;
+const int X = 0;
+const int Y = 1;
+
+double Prompt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+void InputCoefficients(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write($"Введите коэффициенты {i + 1} -го уравнения (y = k1 * x + b1,y = k2 * x + b2)\n");
+        arr[i, B] = Prompt($"Введите коэффициент b: ");
+        arr[i, K] = Prompt($"Введите коэффициент k: ");
+    }
+}
+
+double[] PointCalculation(double[,] coeff)
+{
+    double[] point = new double[2];
+    point[X] = (coeff[1, B] - coeff[0, B]) / (coeff[0, K] - coeff[1, K]); // x=(b2-b1)/(k2-k1)
+    point[Y] = point[X] * coeff[0, 0] + coeff[0, 1];                      //y=x*k1+b1
+    return point;
+}
+
+double[,] cofficient = new double[2, 2];
+
+InputCoefficients(cofficient);
+double[] crossPoint = PointCalculation(cofficient);
+Console.WriteLine();
+Console.WriteLine($"Точка пересечения прямых: ({crossPoint[X]},{crossPoint[Y]})");
